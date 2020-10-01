d0b02d767100f571b0e85f69684acd32f4e1e8f7
Stephen Blair <stephenjohnblair@outlook.com>
2020-10-01 10:05:05 +0100

    Created CommentsList & UpvotesSection components to allow GET and POST server requests via fetch API

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import './App.css';
-import { BrowserRouter as Router, Route} from 'react-router-dom';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import HomePage from './pages/HomePage';
 import AboutPage from './pages/AboutPage';
 import ArticlesListPage from './pages/ArticlesListPage';
@@ -16,11 +16,13 @@ class App extends Component {
       <div className="App">
         <Navbar />
         <div id="page-body">
-          <Route path="/" component={HomePage} exact />
-          <Route path="/about" component={AboutPage}  />
-          <Route path="/articles-list" component={ArticlesListPage}  />
-          <Route path="/article/:name" component={ArticlePage} />
-          <Route component={NotFoundPage} />
+          <Switch>
+            <Route path="/" component={HomePage} exact />
+            <Route path="/about" component={AboutPage}  />
+            <Route path="/articles-list" component={ArticlesListPage}  />
+            <Route path="/article/:name" component={ArticlePage} />
+            <Route component={NotFoundPage} />
+          </Switch>
         </div>
       </div>
       </Router>
--- src/pages/ArticlePage.js
+++ src/pages/ArticlePage.js
@@ -1,23 +1,42 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import articleContent from './article-content';
 import ArticlesList from '../components/ArticlesList';
 import NotFoundPage from './NotFoundPage';
+import CommentsList from '../components/CommentsList';
+import UpvotesSection from '../components/UpvotesSection';
 
 const ArticlePage = ({ match }) => {
+
     const name = match.params.name;
     const article = articleContent.find(article => article.name === name);
+    const [articleInfo, setArticleInfo] = useState({ upvotes: 0, comments: [] });
+
+    useEffect(() => {
+        const fetchData = async () => {
+            const result = await fetch(`/api/articles/${name}`);
+            const body = await result.json();
+            console.log(body);
+            setArticleInfo(body);
+
+        }
+        fetchData();
+    }, [name]);
+    
     if (!article) return {NotFoundPage};
     const otherArticles = articleContent.filter(article  => article.name !== name);
     
 
     return (
         <>
-        <h1>{article.title}</h1>
-        {article.content.map((paragraph, key) => (
-            <p key={key}>{paragraph}</p>
-        ))}
-        <h3>Other Articles:</h3>
-        <ArticlesList articles={otherArticles} />
+            <h1>{article.title}</h1>
+            <UpvotesSection articleName={name} upvotes={articleInfo.upvotes} setArticleInfo={setArticleInfo} />
+            <p>This post has been upvoted {articleInfo.upvotes} times</p>
+            {article.content.map((paragraph, key) => (
+                <p key={key}>{paragraph}</p>
+            ))}
+            <CommentsList comments={articleInfo.comments} />
+            <h3>Other Articles:</h3>
+            <ArticlesList articles={otherArticles} />
         </>
     );
 }
